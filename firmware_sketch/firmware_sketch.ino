49461509a1c36e7c5bc81e6d9c1ca4174fec784e
LorenzoPons <lorenzo.pons@hotmail.it>
2014-08-30 15:54:00 -0400

    added readNTemps and readNAnalogs funcs

--- firmware_sketch/firmware_sketch.ino
+++ firmware_sketch/firmware_sketch.ino
@@ -1,54 +1,76 @@
 
 #include <Adafruit_MAX31855.h>
 
-int tCLK = 2;
-int tCS = 3;
-int tDO = 4;
-int tD1 = 5;
-int tD2 = 6;
-int tD3 = 7;
+// I/O definitions
+#define T_CLK 2
+#define T_CS 3
+#define T_DO 4
+#define T_D1 5
+#define T_D2 6
+#define T_D3 7
+#define VOLTAGE_ANALOG 0
+#define CURRENT_ANALOG 1
+#define PRESSURE_ANALOG 2
 
-Adafruit_MAX31855 thermocouple0(tCLK, tCS, tDO);
-Adafruit_MAX31855 thermocouple1(tCLK, tCS, tD1);
-Adafruit_MAX31855 thermocouple2(tCLK, tCS, tD2);
-Adafruit_MAX31855 thermocouple3(tCLK, tCS, tD3);
+#define PRECISION 2
+#define SEPARATOR ";"
 
+// Function prototypes
+void read4Temps(int[], int, double[], int);
+
+// Global variables
+int dPins[4] = {T_DO, T_D1, T_D2, T_D3};
+int analogInputs[3] = {VOLTAGE_ANALOG, CURRENT_ANALOG, PRESSURE_ANALOG};
+double tCel[4] = {0.0, 0.0, 0.0, 0.0};
+int analogReads[4] = {0, 0, 0, 0};
 
 void setup(){
 
-  Serial.begin(115200);
-  Serial.println("Multiple thermocouple reader");
-  delay(500);
+    Serial.begin(115200);
+    delay(500);
 
 }
 
 void loop(){
   
-  Serial.print("Internal Temp Chip C0 = ");
-  Serial.println(thermocouple0.readInternal());
-  Serial.print("Internal Temp Chip C1 = ");
-  Serial.println(thermocouple1.readInternal());
-  Serial.print("Internal Temp Chip C2 = ");
-  Serial.println(thermocouple2.readInternal());
-  Serial.print("Internal Temp Chip C3 = ");
-  Serial.println(thermocouple3.readInternal());
-  
-  double c0 = thermocouple0.readCelsius();
-  double c1 = thermocouple1.readCelsius();
-  double c2 = thermocouple2.readCelsius();
-  double c3 = thermocouple3.readCelsius();
-   
-  Serial.print("C0 = "); 
-  Serial.println(c0);
-  Serial.print("C1 = "); 
-  Serial.println(c1);
-  Serial.print("C2 = "); 
-  Serial.println(c2);
-  Serial.print("C3 = "); 
-  Serial.println(c3);
-   
-  delay(1000);
+    // Reading sensors
+    readNTemps(dPins, (sizeof(dPins)/sizeof(dPins[0])), tCel, (sizeof(tCel)/sizeof(tCel[0])));
+    
+    readNAnalogs(analogInputs, (sizeof(analogInputs)/sizeof(analogInputs[0])), analogReads);
+    
+    // Sending to serial port
+    for (int i = 0; i < 4; i++)
+    {
+        Serial.print(tCel[i], PRECISION);
+        Serial.print(SEPARATOR);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        Serial.print(analogReads[i]);
+        Serial.print(SEPARATOR);
+    }
+    Serial.println();
+    
+    delay(100);
 
+}
+
+void readNTemps(int pins[], int pLen, double temps[], int tLen)
+{
+    // Cyclic thermocouples reader
+    for (int i = 0; (i < pLen) && (i < tLen); i++)
+    {
+        Adafruit_MAX31855 thermocouple(T_CLK, T_CS, dPins[i]);
+        temps[i] = thermocouple.readCelsius();
+    }
+}
+
+void readNAnalogs(int analogIns[], int inLen, int analogReads[])
+{
+    for (int i = 0; i < inLen; i++)
+        {
+            analogReads[i] = analogRead(analogIns[i]);
+        }
 }
 
 
