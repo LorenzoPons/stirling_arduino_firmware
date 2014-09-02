c040de32daf0949751785545d7c969b06f084c31
LorenzoPons <lorenzo.pons@hotmail.it>
2014-09-01 21:59:36 -0400

    added conversion function from double to string

--- firmware_sketch/firmware_sketch.ino
+++ firmware_sketch/firmware_sketch.ino
@@ -38,6 +38,12 @@ void loop(){
     
     readNAnalogs(analogInputs, (sizeof(analogInputs)/sizeof(analogInputs[0])), analogReads);
     
+    String measurements = String("t0;t1;t2;t3;v;i;p;");
+    String t0 = doubleToString(tCel[0], PRECISION);
+    measurements.replace("t0", t0);
+    Serial.print(measurements + "\n");
+    
+    /*
     // Sending to serial port
     for (int i = 0; i < 4; i++)
     {
@@ -50,6 +56,8 @@ void loop(){
         Serial.print(SEPARATOR);
     }
     Serial.println();
+    
+    */
     
     delay(100);
 
@@ -71,6 +79,55 @@ void readNAnalogs(int analogIns[], int inLen, int analogReads[])
         {
             analogReads[i] = analogRead(analogIns[i]);
         }
+}
+
+String doubleToString(double number, uint8_t digits) 
+{ 
+  String str = String("");
+  
+  if (isnan(number)) return String("nan");
+  if (isinf(number)) return String("inf");
+  if (number > 4294967040.0) return String("ovf");  // constant determined empirically
+  if (number <-4294967040.0) return String("ovf");  // constant determined empirically
+  
+  // Handle negative numbers
+  if (number < 0.0)
+  {
+     str += "-";
+     number = -number;
+  }
+  else
+  {
+     str += "+";
+  }
+
+  // Round correctly so that print(1.999, 2) prints as "2.00"
+  double rounding = 0.5;
+  for (uint8_t i=0; i<digits; ++i)
+    rounding /= 10.0;
+  
+  number += rounding;
+
+  // Extract the integer part of the number and print it
+  unsigned long int_part = (unsigned long)number;
+  double remainder = number - (double)int_part;
+  str += int_part;
+
+  // Print the decimal point, but only if there are digits beyond
+  if (digits > 0) {
+    str += "."; 
+  }
+
+  // Extract digits from the remainder one at a time
+  while (digits-- > 0)
+  {
+    remainder *= 10.0;
+    int toPrint = int(remainder);
+    str += toPrint;
+    remainder -= toPrint; 
+  } 
+  
+  return str;
 }
 
 
