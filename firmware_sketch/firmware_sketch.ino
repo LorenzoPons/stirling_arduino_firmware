47712367851876c7d71f3d55b3ee2500e7561662
LorenzoPons <lorenzo.pons@hotmail.it>
2014-09-01 23:05:12 -0400

    added toVolts conversion function

--- firmware_sketch/firmware_sketch.ino
+++ firmware_sketch/firmware_sketch.ino
@@ -13,10 +13,16 @@
 #define PRESSURE_ANALOG 2
 
 #define PRECISION 2
+#define V_RES 0.00488
+#define R_RATIO 0.025
 #define SEPARATOR ";"
 
 // Function prototypes
 void read4Temps(int[], int, double[], int);
+void readNAnalogs(int[], int, int[]);
+String toVolts(int);
+
+String doubleToString(double, uint8_t);
 
 // Global variables
 int dPins[4] = {T_DO, T_D1, T_D2, T_D3};
@@ -28,7 +34,6 @@ void setup(){
 
     Serial.begin(115200);
     delay(500);
-
 }
 
 void loop(){
@@ -38,16 +43,27 @@ void loop(){
     
     readNAnalogs(analogInputs, (sizeof(analogInputs)/sizeof(analogInputs[0])), analogReads);
     
+    // Sending measurements to serial port
     String measurements = String("t0;t1;t2;t3;v;i;p;");
     
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 7; i++)
     {
-        String temp = doubleToString(tCel[i], PRECISION);
-        String tokenTemp = "t" + String(i);
-        measurements.replace(tokenTemp, temp);
+        if (i < 4)
+        {
+            String temp = doubleToString(tCel[i], PRECISION);
+            String tokenTemp = "t" + String(i);
+            measurements.replace(tokenTemp, temp);
+        }
+        if ((i > 3) && (i < 7))
+        {
+            
+        }
     }
-    
-    Serial.print(measurements + "\n");
+   
+    //Serial.print(measurements + "\n");
+    int current = 511;
+    String strCurrent = toAmpere(current);
+    Serial.print(strCurrent + "\n");
     
     /*
     // Sending to serial port
@@ -87,53 +103,60 @@ void readNAnalogs(int analogIns[], int inLen, int analogReads[])
         }
 }
 
+String toVolts(int adcOut)
+{
+    double dVoltage = (double)adcOut * V_RES * (1/R_RATIO);
+    String voltage = doubleToString(dVoltage, PRECISION);
+    return voltage;
+}
+
 String doubleToString(double number, uint8_t digits) 
 { 
-  String str = String("");
+    String str = String("");
+    
+    if (isnan(number)) return String("nan");
+    if (isinf(number)) return String("inf");
+    if (number > 4294967040.0) return String("ovf");  // constant determined empirically
+    if (number <-4294967040.0) return String("ovf");  // constant determined empirically
+    
+    // Handle negative numbers
+    if (number < 0.0)
+    {
+       str += "-";
+       number = -number;
+    }
+    else
+    {
+       str += "+";
+    }
   
-  if (isnan(number)) return String("nan");
-  if (isinf(number)) return String("inf");
-  if (number > 4294967040.0) return String("ovf");  // constant determined empirically
-  if (number <-4294967040.0) return String("ovf");  // constant determined empirically
+    // Round correctly so that print(1.999, 2) prints as "2.00"
+    double rounding = 0.5;
+    for (uint8_t i=0; i<digits; ++i)
+      rounding /= 10.0;
+    
+    number += rounding;
   
-  // Handle negative numbers
-  if (number < 0.0)
-  {
-     str += "-";
-     number = -number;
-  }
-  else
-  {
-     str += "+";
-  }
-
-  // Round correctly so that print(1.999, 2) prints as "2.00"
-  double rounding = 0.5;
-  for (uint8_t i=0; i<digits; ++i)
-    rounding /= 10.0;
+    // Extract the integer part of the number and print it
+    unsigned long int_part = (unsigned long)number;
+    double remainder = number - (double)int_part;
+    str += int_part;
   
-  number += rounding;
-
-  // Extract the integer part of the number and print it
-  unsigned long int_part = (unsigned long)number;
-  double remainder = number - (double)int_part;
-  str += int_part;
-
-  // Print the decimal point, but only if there are digits beyond
-  if (digits > 0) {
-    str += "."; 
-  }
-
-  // Extract digits from the remainder one at a time
-  while (digits-- > 0)
-  {
-    remainder *= 10.0;
-    int toPrint = int(remainder);
-    str += toPrint;
-    remainder -= toPrint; 
-  } 
+    // Print the decimal point, but only if there are digits beyond
+    if (digits > 0) {
+      str += "."; 
+    }
   
-  return str;
+    // Extract digits from the remainder one at a time
+    while (digits-- > 0)
+    {
+      remainder *= 10.0;
+      int toPrint = int(remainder);
+      str += toPrint;
+      remainder -= toPrint; 
+    } 
+    
+    return str;
 }
 
 
